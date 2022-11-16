01b2c5167f91e6a9eaaf9271192d7569f0333e92
Isaac Kim <kisaac95@yahoo.com>
2022-11-15 19:51:45 -0800

    bugfix

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req,res){
+    res.send('Hello World')
+})
+
+app.get('*', function(req,res) {
+    res.status(404).send('<h1>404 PAGE</h1>') 
+})
+
+
+
+app.listen(process.env.PORT)
