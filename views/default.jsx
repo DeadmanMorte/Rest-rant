6b461275a5252da26946159cb3823171e14ffade
Isaac Kim <kisaac95@yahoo.com>
2022-11-20 21:32:50 -0800

    Navbar w style

--- views/default.jsx
+++ views/default.jsx
@@ -10,6 +10,19 @@ function Def (html) {
             </head>
             <body>
                 <div className='content'>
+                <nav>
+                    <ul>
+                        <li>
+                        <a href="/">Home</a>
+                        </li>
+                        <li>
+                        <a href="/places">Places</a>
+                        </li>
+                        <li>
+                        <a href="/places/new">Add Place</a>
+                        </li>
+                    </ul>
+                </nav>
                     {html.children}
                 </div>
                 <footer className='footer'>STUFFS TO PUT LATER</footer>
