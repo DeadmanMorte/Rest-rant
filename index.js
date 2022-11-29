385fa2343f721cd5ea5bb1c6941d6f3417c7025d
Isaac Kim <kisaac95@yahoo.com>
2022-11-28 21:49:06 -0800

    added a bunch

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // MODULES AND GLOBALS
 require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 const app = express()
 const methodOverride = require('method-override')
 
--- models/places.js
+++ models/places.js
@@ -1,15 +1,27 @@
+const mongoose = require('mongoose')
 
-module.exports = [{
-    name: 'Supreme Grand Master Food',
-    city: 'Huntington Beach',
-    state: 'CA',
-    cuisines: 'Vegan, Pan-Asian',
-    pic: '/images/vegan-nirvana.jpg'
-  }, {
-    name: 'Bad BBQ',
-    city: 'Artesia',
-    state: 'CA',
-    cuisines: 'Korean Barbeque, All You Can Eat (AYCE)',
-    pic: '/images/BAD-BBQ.jpg'
-  }]
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true },
+  city: { type: String, default: 'Anytown' },
+  state: { type: String, default: 'USA' },
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+// [{
+//     name: 'Supreme Grand Master Food',
+//     city: 'Huntington Beach',
+//     state: 'CA',
+//     cuisines: 'Vegan, Pan-Asian',
+//     pic: '/images/vegan-nirvana.jpg'
+//   }, {
+//     name: 'Bad BBQ',
+//     city: 'Artesia',
+//     state: 'CA',
+//     cuisines: 'Korean Barbeque, All You Can Eat (AYCE)',
+//     pic: '/images/BAD-BBQ.jpg'
+//   }]
   
